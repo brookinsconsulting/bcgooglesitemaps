d1370245853a05f96993efa49bffa654ba3b8e35
Brookins Consulting <info@brookinsconsulting.com>
2013-05-27 15:35:56 -0700

    Updated: Updated release documentation

--- ezinfo.php
+++ ezinfo.php
@@ -2,7 +2,7 @@
 /**
  * File containing the all2egooglesitemaps copyright information file
  *
- * @copyright Copyright (C) 2008 all2e GmbH, 1999-2012 Brookins Consutling. All rights reserved.
+ * @copyright Copyright (C) 2008 all2e GmbH, 1999-2013 Brookins Consutling. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
  * @version //autogentag//
  * @package all2egooglesitemaps
@@ -13,8 +13,8 @@ class all2eGoogleSitemapsInfo
     function info()
     {
         return array( 'Name' => "Google Sitemaps Generator",
-                      'Version' => "1.1.0",
-                      'Copyright' => "Copyright (C) 2008 <a href='http:www.all2e.com' title='all2e GmbH'>all2e GmbH</a> and 1999 - 2012 <a href='http://brookinsconsulting.com' title='Brookins Consulting'>Brookins Consulting</a>",
+                      'Version' => "1.1.1",
+                      'Copyright' => "Copyright (C) 2008 <a href='http:www.all2e.com' title='all2e GmbH'>all2e GmbH</a> and 1999 - 2013 <a href='http://brookinsconsulting.com' title='Brookins Consulting'>Brookins Consulting</a>",
                       'License' => "GNU General Public License v2.0"
                      );
     }
