bc003b90ae74fc98ae175ac8c9474e8aef3f2e63
Brookins Consulting <info@brookinsconsulting.com>
2023-01-31 15:56:34 +0000

    Updated: Updated extension version number and added changelog

--- ezinfo.php
+++ ezinfo.php
@@ -12,9 +12,9 @@ class bcGoogleSitemapsInfo
 {
     public static function info()
     {
-        return array( 'Name' => "<a href='http://projects.ez.no/bcgooglesitemaps'>BC Google Sitemaps</a>",
-                      'Version' => "1.1.3",
-                      'Copyright' => "Copyright (C) 1999 - 2014 <a href='http://brookinsconsulting.com' title='Brookins Consulting'>Brookins Consulting</a>, 2008 <a href='http:www.all2e.com' title='all2e GmbH'>all2e GmbH</a> and 2008 <a href='http://www.mediata.net' title='MEDIATA Communications GmbH'>MEDIATA Communications GmbH</a>",
+        return array( 'Name' => "<a href='https://github.com/brookinsconsulting/bcgooglesitemaps'>BC Google Sitemaps</a>",
+                      'Version' => "1.1.4",
+                      'Copyright' => "Copyright (C) 1999 - 2023 <a href='http://brookinsconsulting.com' title='Brookins Consulting'>Brookins Consulting</a>, 2008 <a href='http:www.all2e.com' title='all2e GmbH'>all2e GmbH</a> and 2008 <a href='http://www.mediata.net' title='MEDIATA Communications GmbH'>MEDIATA Communications GmbH</a>",
                       'License' => "GNU General Public License v2.0",
                       'info_url' => "https://github.com/brookinsconsulting/bcgooglesitemaps"
                     );
